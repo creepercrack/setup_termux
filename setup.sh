ba460243b7680f9d7328e4b4f418d54b328f5c74
creepercrack <47305546+creepercrack@users.noreply.github.com>
2022-02-18 01:40:25 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
-termux-setup-storage
 apt update
 apt upgrade
 apt install python ffmpeg
 pip install yt-dlp
-mkdir -p ~/.config/youtube-dl
-echo # Default Output Directory and Pattern >> ~/.config/youtube-dl/config
-echo -o ~/storage/downloads/%(extractor_key)s/%(uploader)s/%(title)s-%(id)s.%(ext)s >> ~/.config/youtube-dl/config
+mkdir -p ~/.config/yt-dlp
+echo # Default Output Directory and Pattern >> ~/.config/yt-dlp/config
+echo -o /storage/7C23-9E17/Vídeos/%(title)s.%(ext)s >> ~/.config/yt-dlp/config
 mkdir ~/bin
 echo #!/bin/bash >> ~/bin/termux-url-opener
 echo url=$1 >> ~/bin/termux-url-opener
-echo youtube-dl $url >> ~/bin/termux-url-opener
+echo yt-dlp -f 'bestvideo[height<=720]+bestaudio/best' $url >> ~/bin/termux-url-opener
 chmod +x ~/bin/termux-url-opener
