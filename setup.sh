c014b4ec9406dfd9c1475714c6be51149bc99cbd
creepercrack <47305546+creepercrack@users.noreply.github.com>
2022-02-18 01:41:35 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-apt update
-apt upgrade
-apt install python ffmpeg
-pip install yt-dlp
+apt update -y
+apt upgrade -y
+apt install -y python ffmpeg
+pip install --yes yt-dlp
 mkdir -p ~/.config/yt-dlp
 echo # Default Output Directory and Pattern >> ~/.config/yt-dlp/config
 echo -o /storage/7C23-9E17/Vídeos/%(title)s.%(ext)s >> ~/.config/yt-dlp/config
