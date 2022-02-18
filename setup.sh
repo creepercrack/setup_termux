3a903a23f985a0594d97e1a31ae23df787253c5f
creepercrack <47305546+creepercrack@users.noreply.github.com>
2022-02-18 01:53:36 +0100

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt update -y
-apt upgrade -y
-apt install -y python ffmpeg
-pip install --yes yt-dlp
-curl https://raw.githubusercontent.com/creepercrack/setup_termux/main/config -o ~/.config/youtube-dl/config
-curl https://raw.githubusercontent.com/creepercrack/setup_termux/main/termux-url-opener -o ~/bin/termux-url-opener
-chmod +x ~/bin/termux-url-opener
