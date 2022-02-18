965398e657a0c75cb0b2c33e073d219cf4d23495
creepercrack <47305546+creepercrack@users.noreply.github.com>
2022-02-18 02:02:19 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ apt update -y
 apt upgrade -y
 apt install -y python ffmpeg
 pip install --yes yt-dlp
+mkdir -p ~/.config/youtube-dl
 curl https://raw.githubusercontent.com/creepercrack/setup_termux/main/config -o ~/.config/youtube-dl/config
+mkdir ~/bin
 curl https://raw.githubusercontent.com/creepercrack/setup_termux/main/termux-url-opener -o ~/bin/termux-url-opener
 chmod +x ~/bin/termux-url-opener
