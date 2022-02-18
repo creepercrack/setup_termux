55f5a1deeacec62ce82aa680547812bf27188aa1
creepercrack <47305546+creepercrack@users.noreply.github.com>
2022-02-18 01:48:16 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -3,11 +3,6 @@ apt update -y
 apt upgrade -y
 apt install -y python ffmpeg
 pip install --yes yt-dlp
-mkdir -p ~/.config/yt-dlp
-echo # Default Output Directory and Pattern >> ~/.config/yt-dlp/config
-echo -o /storage/7C23-9E17/Vídeos/%(title)s.%(ext)s >> ~/.config/yt-dlp/config
-mkdir ~/bin
-echo #!/bin/bash >> ~/bin/termux-url-opener
-echo url=$1 >> ~/bin/termux-url-opener
-echo yt-dlp -f 'bestvideo[height<=720]+bestaudio/best' $url >> ~/bin/termux-url-opener
+curl https://raw.githubusercontent.com/creepercrack/setup_termux/main/config -o ~/.config/youtube-dl/config
+curl https://raw.githubusercontent.com/creepercrack/setup_termux/main/termux-url-opener -o ~/bin/termux-url-opener
 chmod +x ~/bin/termux-url-opener
